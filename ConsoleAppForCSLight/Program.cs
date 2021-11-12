46285e07a1f612403348d49243ab227def4d3af4
moroz <moroz@DESKTOP-HEU300D>
2021-11-12 15:55:32 +0200

    Добавьте файлы проекта.

---
+++ ConsoleAppForCSLight/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForCSLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte strange;
+            int helthHero;
+            uint helthBoss;
+            string heroName;
+            char symbolLeague;
+            bool opportunityToAttack;
+            double coeffAttack;
+            float coeffDefence;
+            ulong countUsers;
+            ushort countClans;
+        }
+    }
+}
