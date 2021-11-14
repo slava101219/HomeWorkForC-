cfaa268415d79d120c88324bda9d232cac019270
slava101219 <morozko.slava@mail.ru>
2021-11-14 02:45:45 +0200

    wer

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -10,12 +10,39 @@ namespace ConsoleAppForCSLight
     {
         static void Main(string[] args)
         {
-            int sizeForSeries = 3;
-            int numberOfPictures = 52;
-            int unnecessaryPictures = 52 % 3;
-            int numberOfFullSeries = (52 - unnecessaryPictures) / 3;
-            Console.WriteLine(unnecessaryPictures);
-            Console.WriteLine(numberOfFullSeries);
+            int priceCrystal = 16;
+            Console.WriteLine("Приветствую в нашем магазине. Хотите купить кристаллы? Отлично, сегодня всего по " + priceCrystal + 
+                "! Сколько золота вы готовы потратить?");
+            int myGold = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+
+                if (myGold < priceCrystal)
+                {
+                    Console.WriteLine("Я не могу разбить кристалл( Нужно потратить немного больше.");
+                    myGold = Convert.ToInt32(Console.ReadLine());
+                }
+                else break;
+                }               
+            int crystalsAvailableForPurchase = myGold / priceCrystal;
+            Console.WriteLine("За свои " + myGold + " золота, вы можете купить " + crystalsAvailableForPurchase +
+            " кристалла. Сколько же кристалов вам требуется?");
+            int myCrystals = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                if (myCrystals > crystalsAvailableForPurchase)
+                {
+                    Console.WriteLine("Вам нехватит золота( купите чуть меньше.");
+                    myCrystals = Convert.ToInt32(Console.ReadLine());
+                }
+                else break;
+            }
+            int finalGold = myGold - (priceCrystal * myCrystals);
+            if (myCrystals == 0)
+            {
+                Console.WriteLine("Жаль. Теперь у вас " + myCrystals + " кристалов. У вас осталось " + finalGold + " золота.");
+            }
+            Console.WriteLine("Отлично! Теперь у вас " + myCrystals + " кристалов. У вас осталось " + finalGold + " золота.");
         }
         
     }
