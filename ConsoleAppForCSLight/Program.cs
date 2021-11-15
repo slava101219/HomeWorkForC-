18eafd67d55ebed447a2d7fb8f2c8467a6ee262f
slava101219 <morozko.slava@mail.ru>
2021-11-15 14:25:34 +0200

    sdf

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -10,13 +10,15 @@ namespace ConsoleAppForCSLight
     {
         static void Main(string[] args)
         {
-            int grannyCount, hour, minute;
+            int grannyCount;
+            int waitingOfHour;
+            int waitingOfMinute;
             int waitTime = 10;
             Console.WriteLine("введите кол-во старушек");
             grannyCount = Convert.ToInt32(Console.ReadLine());
-            minute = (grannyCount * waitTime) % 60;
-            hour = (grannyCount * waitTime - minute) / 60;
-            Console.WriteLine("вы должны отстоять в очереди " + hour + " часов и " + minute + " минут.");
+            waitingOfMinute = (grannyCount * waitTime) % 60;
+            waitingOfHour = (grannyCount * waitTime) / 60;
+            Console.WriteLine("вы должны отстоять в очереди " + waitingOfHour + " часов и " + waitingOfMinute + " минут.");
         }
         
     }
