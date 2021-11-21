6b66512cd2cc16c840d00de4f4aec99e31a29841
slava101219 <morozko.slava@mail.ru>
2021-11-21 18:20:09 +0200

    2 consil menu

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -12,41 +12,40 @@ namespace ConsoleAppForCSLight
         {
             int valueForConsole;
             string comand;
-            string menu = "команды: getWidth - установить ширину консоли; getHeight - установить высоту консоли; " +
+            string menu = "команды: getWidth - установить ширину консоли; setWidth - установить ширину кончоли; getHeight - установить высоту консоли; " +
                 "getDateTime - вывести сегодняшнюю дату и время; getCountDaysToday - вывести количество дней с начала года;" +
                 " exit - выйти из программы";
             Console.WriteLine(menu);
             comand = Console.ReadLine();
             while (comand != "exit")
             {
+                Console.WriteLine(menu);
+                comand = Console.ReadLine();
 
                 switch (comand)
                 {
                     case "getWidth":
                         valueForConsole = Console.WindowWidth;
                         Console.WriteLine(valueForConsole);
-                        Console.WriteLine(menu);
-                        comand = Console.ReadLine();
+                        break;
+                    case "setWidth":
+                        Console.WriteLine("какое значение установить?");
+                        valueForConsole = Convert.ToInt32(Console.ReadLine());
+                        Console.WindowWidth = valueForConsole;
+                        Console.WriteLine("изменено на : " + Console.WindowWidth);
                         break;
                     case "getHeight":
                         valueForConsole = Console.WindowHeight;
                         Console.WriteLine(valueForConsole);
-                        Console.WriteLine(menu);
-                        comand = Console.ReadLine();
                         break;
                     case "getDateTime":
                         Console.WriteLine(DateTime.Now);
-                        Console.WriteLine(menu);
-                        comand = Console.ReadLine();
                         break;
                     case "getCountDaysToday":
                         Console.WriteLine(DateTime.Today.DayOfYear);
-                        Console.WriteLine(menu);
-                        comand = Console.ReadLine();
                         break;
                     default:
                         Console.WriteLine("это не команда");
-                        comand = Console.ReadLine();
                         break;
                 }
 
