88945ff4af403fed10fe332478088fbca2ca93e3
slava101219 <morozko.slava@mail.ru>
2021-11-21 11:20:41 +0200

    4й конвертер

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -21,62 +21,92 @@ namespace ConsoleAppForCSLight
             double exchangeRateRubToUsd = 0.014;
             double exchangeRateRubToEur = 0.012;
             string selectionUser = "0";
-            string spaceNine = "         ";
+            string spacesForArray = "         ";
 
             while (selectionUser != "7")
             {
                 Console.WriteLine("usd : " + usdCount + " eur : " + eurCount + " rub : " + rubCount);
-                Console.WriteLine("ввести : 1 для usd => eur;\n" + spaceNine + "2 для usd => rub;\n" + spaceNine + "3 для eur => usd;\n" +
-                spaceNine + "4 для eur => rub;\n" + spaceNine + "5 для rub => usd;\n" + spaceNine + "6 для rub => eur;\n" + spaceNine +
+                Console.WriteLine("ввести : 1 для usd => eur;\n" + spacesForArray + "2 для usd => rub;\n" + spacesForArray + "3 для eur => usd;\n" +
+                spacesForArray + "4 для eur => rub;\n" + spacesForArray + "5 для rub => usd;\n" + spacesForArray + "6 для rub => eur;\n" + spacesForArray +
                 "7 для выхода");
                 selectionUser = Console.ReadLine();
-
+                
                 if (selectionUser == "1")
                 {
                     Console.WriteLine("сколько меняем?");
                     changeMoneyCount = Convert.ToDouble(Console.ReadLine());
+                    while (changeMoneyCount > usdCount || changeMoneyCount < 0)
+                    {
+                        Console.WriteLine("Не верная сумма. Еще раз.");
+                        changeMoneyCount = Convert.ToDouble(Console.ReadLine());
+                    }
                     usdCount -= changeMoneyCount;
-                    eurCount = eurCount + changeMoneyCount * exchangeRateUsdToEur;                                       
+                    eurCount += changeMoneyCount * exchangeRateUsdToEur;                                       
                 }
 
                 else if (selectionUser == "2")
                 {
                     Console.WriteLine("сколько меняем?");
                     changeMoneyCount = Convert.ToDouble(Console.ReadLine());
+                    while (changeMoneyCount > usdCount || changeMoneyCount < 0)
+                    {
+                        Console.WriteLine("Не верная сумма. Еще раз.");
+                        changeMoneyCount = Convert.ToDouble(Console.ReadLine());
+                    }
                     usdCount -= changeMoneyCount;
-                    rubCount = rubCount + changeMoneyCount * exchangeRateUsdToRub;
+                    rubCount += changeMoneyCount * exchangeRateUsdToRub;
                 }
 
                 else if (selectionUser == "3")
                 {
                     Console.WriteLine("сколько меняем?");
                     changeMoneyCount = Convert.ToDouble(Console.ReadLine());
+                    while (changeMoneyCount > eurCount || changeMoneyCount < 0)
+                    {
+                        Console.WriteLine("Не верная сумма. Еще раз.");
+                        changeMoneyCount = Convert.ToDouble(Console.ReadLine());
+                    }
                     eurCount -= changeMoneyCount;
-                    usdCount = usdCount + changeMoneyCount * exchangeRateEurToUsd;
+                    usdCount += changeMoneyCount * exchangeRateEurToUsd;
                 }
 
                 else if (selectionUser == "4")
                 {
                     Console.WriteLine("сколько меняем?");
                     changeMoneyCount = Convert.ToDouble(Console.ReadLine());
+                    while (changeMoneyCount > eurCount || changeMoneyCount < 0)
+                    {
+                        Console.WriteLine("Не верная сумма. Еще раз.");
+                        changeMoneyCount = Convert.ToDouble(Console.ReadLine());
+                    }
                     eurCount -= changeMoneyCount;
-                    rubCount = rubCount + changeMoneyCount * exchangeRateEurToRub;
+                    rubCount += + changeMoneyCount * exchangeRateEurToRub;
                 }
 
                 else if (selectionUser == "5")
                 {
                     Console.WriteLine("сколько меняем?");
                     changeMoneyCount = Convert.ToDouble(Console.ReadLine());
+                    while (changeMoneyCount > rubCount || changeMoneyCount < 0)
+                    {
+                        Console.WriteLine("Не верная сумма. Еще раз.");
+                        changeMoneyCount = Convert.ToDouble(Console.ReadLine());
+                    }
                     rubCount -= changeMoneyCount;
-                    usdCount = usdCount + changeMoneyCount * exchangeRateRubToUsd;
+                    usdCount += + changeMoneyCount * exchangeRateRubToUsd;
                 }
 
                 else if (selectionUser == "6")
                 {
                     Console.WriteLine("сколько меняем?");
                     changeMoneyCount = Convert.ToDouble(Console.ReadLine());
+                    while (changeMoneyCount > rubCount || changeMoneyCount < 0)
+                    {
+                        Console.WriteLine("Не верная сумма. Еще раз.");
+                        changeMoneyCount = Convert.ToDouble(Console.ReadLine());
+                    }
                     rubCount -= changeMoneyCount;
-                    eurCount = eurCount + changeMoneyCount * exchangeRateRubToEur;
+                    eurCount += + changeMoneyCount * exchangeRateRubToEur;
                 }
             }
             Console.WriteLine("досвидания");
