7991a62deec1e291afcfb12ca106cd3822ff6c32
slava101219 <morozko.slava@mail.ru>
2021-11-22 15:31:11 +0200

    asd

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -10,48 +10,10 @@ namespace ConsoleAppForCSLight
     {
         static void Main(string[] args)
         {
-            int valueForConsole;
-            string comand;
-            string menu = "команды: getWidth - установить ширину консоли; setWidth - установить ширину кончоли; getHeight - установить высоту консоли; " +
-                "getDateTime - вывести сегодняшнюю дату и время; getCountDaysToday - вывести количество дней с начала года;" +
-                " exit - выйти из программы";
-            Console.WriteLine(menu);
-            comand = Console.ReadLine();
-            while (comand != "exit")
+            for (int i = 7; i<=98; i+=7)
             {
-                Console.WriteLine(menu);
-                comand = Console.ReadLine();
-
-                switch (comand)
-                {
-                    case "getWidth":
-                        valueForConsole = Console.WindowWidth;
-                        Console.WriteLine(valueForConsole);
-                        break;
-                    case "setWidth":
-                        Console.WriteLine("какое значение установить?");
-                        valueForConsole = Convert.ToInt32(Console.ReadLine());
-                        Console.WindowWidth = valueForConsole;
-                        Console.WriteLine("изменено на : " + Console.WindowWidth);
-                        break;
-                    case "getHeight":
-                        valueForConsole = Console.WindowHeight;
-                        Console.WriteLine(valueForConsole);
-                        break;
-                    case "getDateTime":
-                        Console.WriteLine(DateTime.Now);
-                        break;
-                    case "getCountDaysToday":
-                        Console.WriteLine(DateTime.Today.DayOfYear);
-                        break;
-                    default:
-                        Console.WriteLine("это не команда");
-                        break;
-                }
-
-            }
-            
-
+                Console.WriteLine(i);
+            }                      
         }       
     }
 }
