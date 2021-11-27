0b85b7bc9b307d29fac5978fe2d0b57e958e5d48
slava101219 <morozko.slava@mail.ru>
2021-11-27 17:10:23 +0200

    sdf

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -10,93 +10,32 @@ namespace ConsoleAppForCSLight
     {
         static void Main(string[] args)
         {
-            double healthHero = 1000;
-            double HealthBoss = 10000;
-            double attackHero = 200;
-            double attackBoss = 100;
-            double defenceHero = 70;
-            double defenceBoss = 140;
-            double damageToHero = attackBoss / defenceHero * attackBoss;
-            double damageForOneSkill; 
-            double damageForTwoSkill;
-            double currentDamage = 0;
-            double coeffDefensIgnore = 0.8;
-            double coeffdefenceReduction = 0.95;
-            int cooldownForTwoSkill = 0;
-            int cooldownForThreeSkill = 0;
-            string selectSkill;
-            Console.WriteLine("To battle!");
+            int sum = 0;
+            int mult = 1;
+            Random rand = new Random();
+            int[,] array = new int [ 5, 5 ];
 
-            while (HealthBoss > 0 && healthHero > 0)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                Console.WriteLine("1 - обычный удар. без отката.");
-                Console.WriteLine("2 - Укус вампира. игнорирует 20% защиты врага. 50% урона крадет в виде здоровья. откат 2 хода");
-                Console.WriteLine("3 - прилив ярости. навсегда сокращает защиту врага на 5%. откат 4 хода.");
-                selectSkill = Console.ReadLine();
-
-                while (selectSkill != "1" && selectSkill != "2" && selectSkill != "3")
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    Console.WriteLine("не выбран навык.");
-                    selectSkill = Console.ReadLine();
-                }
+                    array[i, j] = rand.Next(0, 10);
 
-                while (selectSkill == "2" && cooldownForTwoSkill > 0 || selectSkill == "3" && cooldownForThreeSkill > 0)
-                {
-                    Console.WriteLine("не выбран навык.");
-                    selectSkill = Console.ReadLine();
+                    if (i == 1)
+                    {
+                        sum += array[i, j];
+                    }
+                    
+                    if (j == 0)
+                    {
+                        mult *= array[i, j];
+                    }
+                    Console.Write(array[i, j] + " ");
                 }
-
-                switch (selectSkill)
-                {
-                    case "1":
-                        damageForOneSkill = attackHero / defenceBoss * attackHero;
-                        HealthBoss -= damageForOneSkill;
-                        currentDamage = damageForOneSkill;
-                        break;
-                    case "2":
-                        damageForTwoSkill = attackHero / (defenceBoss * coeffDefensIgnore) * attackHero;
-                        HealthBoss -= damageForTwoSkill;
-                        currentDamage = damageForTwoSkill;
-                        healthHero += damageForTwoSkill;
-                        cooldownForTwoSkill = 3;
-                        break;
-                    case "3":
-                        defenceBoss = defenceBoss * coeffdefenceReduction;
-                        damageForOneSkill = attackHero / defenceBoss * attackHero;
-                        HealthBoss -= damageForOneSkill;
-                        currentDamage = damageForOneSkill;
-                        cooldownForThreeSkill = 5;
-                        break;
-                }
-
-                Console.WriteLine("Вы нанесли " + ((int)currentDamage) + " урона." + "у босса осталось " + ((int)HealthBoss) + " здоровья.");
-                healthHero -= damageToHero;
-
-                if (healthHero > 1000)
-                {
-                    healthHero = 1000;
-                }
-                Console.WriteLine("босс нанес вам " + ((int)damageToHero) + " урона. у вас осталось " + ((int)healthHero) + " здоровья.");
-
-                if (cooldownForTwoSkill != 0)
-                {
-                    cooldownForTwoSkill--;
-                }
-
-                if (cooldownForThreeSkill != 0)
-                {
-                    cooldownForThreeSkill--;
-                }
-
-                if (HealthBoss <= 0)
-                {
-                    Console.WriteLine("You WIN!!!!!");
-                }
-                else if (healthHero <= 0)
-                {
-                    Console.WriteLine("You Lose:(");
-                }
+                Console.WriteLine();
             }
+
+            Console.WriteLine(sum + " | " + mult);
 
         }
     }
