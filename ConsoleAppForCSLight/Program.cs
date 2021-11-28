62052357d21ae337033858563cbb6e5dafd110af
slava101219 <morozko.slava@mail.ru>
2021-11-28 17:00:57 +0200

    array1

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -11,31 +11,27 @@ namespace ConsoleAppForCSLight
         static void Main(string[] args)
         {
             int sum = 0;
-            int mult = 1;
-            Random rand = new Random();
+            int multiplication = 1;
+            Random random = new Random();
             int[,] array = new int [ 5, 5 ];
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    array[i, j] = rand.Next(0, 10);
-
-                    if (i == 1)
-                    {
-                        sum += array[i, j];
-                    }
-                    
-                    if (j == 0)
-                    {
-                        mult *= array[i, j];
-                    }
+                    array[i, j] = random.Next(0, 10);
                     Console.Write(array[i, j] + " ");
                 }
                 Console.WriteLine();
             }
 
-            Console.WriteLine(sum + " | " + mult);
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                sum += array[1, i];
+                multiplication *= array[i, 0];
+            }
+
+            Console.WriteLine(sum + " | " + multiplication);
 
         }
     }
