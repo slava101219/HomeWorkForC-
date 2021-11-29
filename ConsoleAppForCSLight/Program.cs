99bc2944bce3decfdfa4cc31e3cf35f7f1865c13
slava101219 <morozko.slava@mail.ru>
2021-11-29 16:38:50 +0200

    sdf

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -29,7 +29,9 @@ namespace ConsoleAppForCSLight
                 Console.WriteLine();
             }
 
-            for(int i = 0; i < array.GetLength(0); i++)
+            Console.WriteLine(maxValue);
+
+            for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
