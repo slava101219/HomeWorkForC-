60152d87aeb07bc056e4d07c58ed3bfa8126060a
slava101219 <morozko.slava@mail.ru>
2021-12-13 12:31:10 +0200

    SDf

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -10,21 +10,105 @@ namespace ConsoleAppForCSLight
     {
         static void Main(string[] args)
         {
-            ConvertStringAndReturnNumber();
+            Console.CursorVisible = false;
+            string hero = "S";
+            int yHero = 30;
+            int xHero = 10;
+            int dyHero = 0;
+            int dxHero = 0;
+            bool isPlaying = true;
+            string[,] map = FillAndDrowMap(20, 100);
+
+            while (isPlaying)
+            {
+                Console.SetCursorPosition(yHero, xHero);
+                Console.Write(hero);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    switch(key.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            dxHero = -1; dyHero = 0;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            dxHero = 1; dyHero = 0;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            dxHero = 0; dyHero = -1;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            dxHero = 0; dyHero = 1;
+                            break;
+                    }
+
+                    if (map[dxHero + xHero, yHero + dyHero] != "|" && map[dxHero + xHero, yHero + dyHero] != "-")
+                    {
+                        Console.SetCursorPosition(yHero, xHero);
+                        Console.Write(" ");
+                        xHero += dxHero;
+                        yHero += dyHero;
+                        Console.SetCursorPosition(yHero, xHero);
+                        Console.Write(hero);
+                    }                   
+                }
+S               
+            }
         }
 
-        static int ConvertStringAndReturnNumber()
+        static string[,] FillAndDrowMap(int height, int width)
         {
-            int returnValue = 0;
-            bool exitFromMethod = false;
-              
-            while (exitFromMethod == false)
-            {
-                string inputString = Console.ReadLine();
-                exitFromMethod = int.TryParse(inputString, out returnValue);
-            }
+            string[,] map = new string[height, width];
 
-            return returnValue;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (i == 0)
+                    {
+                        map[i, j] = "-";
+                        Console.Write(map[i, j]);
+                    }
+                    else if (i == height-1)
+                    {
+                        map[i, j] = "-";
+                        Console.Write(map[i, j]);
+                    }
+                    else if (i > 0 && i < height - 1 && j == 0 )
+                    {
+                        map[i, j] = "|";
+                        Console.Write(map[i, j]);
+                    }
+                    else if (i > 0 && i < height - 1 && j == width - 1)
+                    {
+                        map[i, j] = "|";
+                        Console.Write(map[i, j]);
+                    }
+                    else
+                    {
+                        map[i, j] = " ";
+                        Console.Write(map[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+            return map;
+        }
+
+        static void DrowMap (string[,] map)
+        {
+            int strngsInMap = map.GetUpperBound(0) + 1;
+            int charsInString = map.Length / strngsInMap;
+            for (int i = 0; i < strngsInMap; i++)
+            {
+                for (int j = 0; j < charsInString; j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
