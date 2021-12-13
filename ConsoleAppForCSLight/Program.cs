4779af3569bc827188fd1fe61da49fa2a0abce27
slava101219 <morozko.slava@mail.ru>
2021-12-13 18:18:50 +0200

    sdf

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -29,6 +29,7 @@ namespace ConsoleAppForCSLight
         static void ShuffleArray(ref string[] letters)
         {
             Random random = new Random();
+
             for (int i = letters.Length - 1; i >= 0; i--)
             {
                 int randomItem = random.Next(i);
@@ -37,23 +38,5 @@ namespace ConsoleAppForCSLight
                 letters[i] = shuffledElement;
             }
         }
-
-        /*static void ShuffleArray (ref string[] letters)
-        {
-            Random rand = new Random();
-            string[] lettersCopy = letters;
-            int[] randomIndexsArray = new int[10];
-            int randomIndex;
-
-            for (int i = 0; i < 10; i++)
-            {
-                randomIndex = rand.Next(0, 9);
-                if (!randomIndexsArray.Contains(randomIndex))
-                {
-                    letters[i] = lettersCopy[randomIndex];
-                    randomIndexsArray[i] = randomIndex;
-                }
-            }
-        }*/
     }
 }
