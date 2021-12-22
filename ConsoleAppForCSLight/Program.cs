e1a54d630b9ec5c40744fd55a9b00008be013f60
slava101219 <morozko.slava@mail.ru>
2021-12-22 15:17:38 +0200

    sdf

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -11,76 +11,23 @@ namespace ConsoleAppForCSLight
 
         static void Main(string[] args)
         {
-            string choice;
-            List<string> dossiers = new List<string>();
-            List<string> positionsHeld = new List<string>();
-            bool isWork = true;
-            
-            while(isWork)
-            {              
-                Console.WriteLine("1)добавить досье 2) вывести все досье 3) удалить досье 4) выход");
-                choice = Console.ReadLine();
+            Player player = new Player(20);
+            player.ShowInfo();
+        }
+    }
 
-                switch (choice)
-                {
-                    case "1":
-                        AddNewDossier(dossiers, positionsHeld);
-                        break;
-                    case "2":
-                        ShowAllDossiers(dossiers, positionsHeld);
-                        break;
-                    case "3":
-                        DeleteFromDossiers(Console.ReadLine(), dossiers, positionsHeld);
-                        break;
-                    case "4":
-                        isWork = false;
-                        break;
-                    default:
-                        Console.WriteLine("неверный ввод");
-                        break;
-                }
-                
-            }
+    class Player
+    {
+        public int Lvl;
+
+        public Player(int lvl)
+        {
+            Lvl = lvl;
         }
 
-        static void DeleteFromDossiers (string choice, List<string> dossiers, List<string> positionsHeld)
+        public void ShowInfo()
         {
-            int numberDelete;
-            Console.WriteLine("введите номер удаляемого досье");
-
-            if (int.TryParse(choice, out numberDelete))
-            {
-                if (numberDelete > dossiers.Count || numberDelete < 1)
-                {
-                    Console.WriteLine("такого досье нет.");
-                }
-                else
-                {
-                    dossiers.RemoveAt(numberDelete - 1);
-                    positionsHeld.RemoveAt(numberDelete - 1);
-                }
-            }
-            else
-            {
-                Console.WriteLine("нужно ввести номер");
-            }
-        }
-
-        static void AddNewDossier(List<string> dossiers, List<string> positionsHeld)
-        {
-            Console.WriteLine("введите фио");
-            dossiers.Add(Console.ReadLine());
-            Console.WriteLine("введите должность");
-            positionsHeld.Add(Console.ReadLine());
-        }
-
-        static void ShowAllDossiers (List<string> dossiers, List<string> positionsHeld)
-        {
-            for (int i = 0; i < dossiers.Count; i++)
-            {
-                Console.Write((i + 1) + " " + dossiers[i] + " - " + positionsHeld[i] + "; ");
-            }
-            Console.WriteLine();
+            Console.WriteLine("игрок " + Lvl + " уровня.");
         }
     }
 }
