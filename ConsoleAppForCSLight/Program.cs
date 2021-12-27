9058f2ae7d353bf7e58f823fe3d64eee039bea48
slava101219 <morozko.slava@mail.ru>
2021-12-27 03:26:12 +0200

    dsf

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -11,37 +11,135 @@ namespace ConsoleAppForCSLight
 
         static void Main(string[] args)
         {
-            Renderer render = new Renderer(new Player(30, 10, "$"));
-            render.DrowObject();
+            Database dataBasePlayers = new Database(new List<Player>());
+            bool isWork = true;
+            string choice;
+
+            while (isWork)
+            {
+                ShowMenu();
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        dataBasePlayers.ShowPlayersDB();
+                        break;
+                    case "2":
+                        Console.WriteLine("введите ник для нового игрока");
+                        dataBasePlayers.AddPlayerDB(new Player(Console.ReadLine()));
+                        break;
+                    case "3":
+                        Console.WriteLine("введите номер игрока для изменения статуса бана.");
+
+                        if (int.TryParse(Console.ReadLine(), out int result))
+                        {
+                            dataBasePlayers.BannedPlayerDB(result);
+                        }
+                        else
+                        {
+                            Console.WriteLine("ошибка ввода.");
+                        }
+                        break;
+                    case "4":
+                        Console.WriteLine("введите номер удаляемого игрока.");
+
+                        if (int.TryParse(Console.ReadLine(), out int result2))
+                        {
+                            dataBasePlayers.DeletePlayerDB(result2);
+                        }
+                        else
+                        {
+                            Console.WriteLine("ошибка ввода.");
+                        }
+                        break;
+                    case "5":
+                        isWork = false;
+                        break;
+                    default:
+                        Console.WriteLine("пункт в меню не найден");
+                        break;
+                }
+            }
         }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("1) вывод базы. 2) добавление игрока. 3) забанить/разбанить игрока. 4) удалить игрока. 5) выйти.");
+        }
+
+
     }
 
     class Player
     {
-        public int XСoordinate { get; }
-        public int YСoordinate { get; }
-        public string Logo { get; }
+        public int _lvl { get; private set; } = 1;
+        public string _nickname { get; private set; }
+        public bool _isBanned { get; private set; } = false;
 
-        public Player(int xСoordinate, int yСoordinate, string logo)
+        public Player(string nickname)
         {
-            XСoordinate = xСoordinate;
-            YСoordinate = yСoordinate;
-            Logo = logo;
+            _nickname = nickname;
+        }
+
+        public void BannedPlayer()
+        {
+            _isBanned = !_isBanned;
         }
     }
 
-    class Renderer
+    class Database
     {
-        private Player _player;
-        public Renderer(Player player)
+        private List<Player> DBPlayers;
+
+        public Database(List<Player> dBPlayers)
         {
-            _player = player;
+            DBPlayers = dBPlayers;
         }
 
-        public void DrowObject()
+        public void ShowPlayersDB()
         {
-            Console.SetCursorPosition(_player.XСoordinate, _player.YСoordinate);
-            Console.Write(_player.Logo);
+            foreach(Player player in DBPlayers)
+            {
+                Console.Write((DBPlayers.IndexOf(player) + 1) + ") " + player._nickname + ", " + player._lvl + " lvl, ");
+                if (player._isBanned == false)
+                {
+                    Console.WriteLine("не забанен.");
+                }
+                else
+                {
+                    Console.WriteLine("забанен.");
+                }
+            }
+        }
+
+        public void AddPlayerDB(Player player)
+        {
+            DBPlayers.Add(player);
+        }
+
+        public void BannedPlayerDB (int numberPlayer)
+        {
+            if(numberPlayer > 0 && numberPlayer <= DBPlayers.Count)
+            {
+                DBPlayers[numberPlayer - 1].BannedPlayer();
+            }
+            else
+            {
+                Console.WriteLine("игрок не найден");
+            }          
+        }
+
+        public void DeletePlayerDB (int numberPlayer)
+        {
+            if (numberPlayer > 0 && numberPlayer <= DBPlayers.Count)
+            {
+                DBPlayers.RemoveAt(numberPlayer - 1);
+            }
+            else
+            {
+                Console.WriteLine("игрок не найден");
+            }
         }
     }
 }
