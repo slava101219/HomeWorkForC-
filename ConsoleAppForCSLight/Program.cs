265dcc86edf54eb35454aca47dd788ad0a6202f1
slava101219 <morozko.slava@mail.ru>
2022-01-25 13:54:58 +0200

    sdf

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -12,75 +12,29 @@ namespace ConsoleAppForCSLight
         static void Main(string[] args)
         {
             Zoo zoo = new Zoo();
-            bool IsWork = true;
-            String choice;
-
-            while(IsWork)
-            {
-                Console.Clear();
-                Console.WriteLine("Выбери клетку для просмотра животных. Всего 4 клетки. или exit для выхода.");
-                choice = Console.ReadLine();
-
-                switch(choice)
-                {
-                    case "1":
-                        zoo.ShowInfoCell(zoo.GetCell(choice));
-                        break;
-                    case "2":
-                        zoo.ShowInfoCell(zoo.GetCell(choice));
-                        break;
-                    case "3":
-                        zoo.ShowInfoCell(zoo.GetCell(choice));
-                        break;
-                    case "4":
-                        zoo.ShowInfoCell(zoo.GetCell(choice));
-                        break;
-                    case "exit":
-                        IsWork = false;
-                        break;
-                    default:
-                        break;
-                }
-            }
+            zoo.Start(); 
         }
     }
 
-    class Zoo
+    class Cell
     {
-        private List<Animal> _bisons = new List<Animal>() { new Bison(), new Bison() };
-        private List<Animal> _bulls = new List<Animal>() { new Bull(), new Bull() };
-        private List<Animal> _horses = new List<Animal>() { new Horse(), new Horse(), new Horse() } ;
-        private List<Animal> _deers = new List<Animal>() { new Deer(), new Deer(), new Deer(), new Deer() };
+        private List<List<Animal>> _cells = new List<List<Animal>>() 
+        { 
+            new List<Animal>() { new Bison(), new Bison() }, 
+            new List<Animal>() { new Bull(), new Bull() },
+            new List<Animal>() { new Horse(), new Horse(), new Horse() },
+            new List<Animal>() { new Deer(), new Deer(), new Deer(), new Deer() } 
+        };
 
-        public List<Animal> GetCell(string choice)
+        public void ShowInfoCell(int cellNumber)
         {
-            if (choice == "1")
-            {
-                return _bisons;
-            }
-            else if (choice == "2")
-            {
-                return _bulls;
-            }
-            else if (choice == "3")
-            {
-                return _horses;
-            }
-            else
-            {
-                return _deers;
-            }
-        }
-
-        public void ShowInfoCell(List<Animal> cell)
-        {
-            Console.WriteLine("в клетке " + cell.Count + " животных. Из них " + CountingNumberMales(cell) + " самцы.");
-            Console.WriteLine("Слышны звуки : " + cell[0].ToString());
+            Console.WriteLine("в клетке " + _cells[cellNumber].Count() + " животных. Из них " + CountingNumberMales(_cells[cellNumber]) + " самцы.");
+            Console.WriteLine("Слышны звуки : " + _cells[cellNumber][0].ToString());
             Console.Write("Животным по ");
 
-            foreach(Animal animal in cell)
+            foreach (Animal animal in _cells[cellNumber])
             {
-                Console.Write(animal.GetAge() + ", ");
+                Console.Write(animal.Age + ", ");
             }
 
             Console.Write("лет соответственно.");
@@ -93,37 +47,67 @@ namespace ConsoleAppForCSLight
 
             foreach (Animal animal in cell)
             {
-                malesCount += animal.GetSex();
+                malesCount += animal.Sex;
             }
 
             return malesCount;
         }
+
+        public int GetCellCount()
+        {
+            return _cells.Count();
+        }
+    }
+
+    class Zoo
+    {
+        private Cell cell = new Cell();
+        public void Start()
+        {
+            bool IsWork = true;
+            String choice;
+
+            while (IsWork)
+            {
+                Console.Clear();
+                Console.WriteLine("Выбери клетку для просмотра животных. Всего " + cell.GetCellCount() + " клетки. или exit для выхода.");
+                choice = Console.ReadLine();
+
+                if (int.TryParse(choice, out int result))
+                {
+                    if(result > 0 && result <= cell.GetCellCount())
+                    {
+                        cell.ShowInfoCell(result - 1);
+                    }
+                }
+                else if (choice == "exit")
+                {
+                    IsWork = false;
+                }
+            }
+        }
+
+        
     }
 
     class Animal
     {
-        static Random random = new Random();
+        private static Random _random = new Random();
         private int _male = 2;
         private int _minAge = 1;
         private int _maxAge = 6;
-        protected int Sex;
-        protected int Age { get; private set; }
-        protected string Sound { get; private set; }
+        public int Sex { get; private set; }
+        public int Age { get; private set; }
 
         public Animal()
         {
-            Sex = random.Next(0, _male);
-            Age = random.Next(_minAge, _maxAge);
+            Sex = _random.Next(0, _male);
+            Age = _random.Next(_minAge, _maxAge);
         }
 
-        public int GetSex()
+        public void SetSexMale()
         {
-            return Sex;
-        }
-
-        public int GetAge()
-        {
-            return Age;
+            Sex = 1;
         }
     }
 
@@ -143,7 +127,7 @@ namespace ConsoleAppForCSLight
     {
         public Bull() : base()
         {
-            Sex = 1;
+            SetSexMale();
         }
 
         public override string ToString()
