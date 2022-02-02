3ef250958b8f9bf6a45a9661b4513cf340dc4c3b
slava101219 <morozko.slava@mail.ru>
2022-02-02 15:59:39 +0200

    df

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -11,189 +11,142 @@ namespace ConsoleAppForCSLight
     {
         static void Main(string[] args)
         {
-            string choice;
+            DatabaseCriminals databaseCriminals = new DatabaseCriminals();
             bool isWork = true;
-            CarServiceStation carServiceStation = new CarServiceStation();
+            string choice;
 
             while(isWork == true)
             {
-                Console.WriteLine("1) принять новую машину. 2) купить деталь. 3) просмотр склада. 4) выход.");
+                Console.WriteLine("1) показать всех преступников.");
+                Console.WriteLine("2) показать разыскиваемых преступников, отсортированных по росту.");
+                Console.WriteLine("3) показать разыскиваемых преступников, отсортированных по весу.");
+                Console.WriteLine("4) показать разыскиваемых преступников, отсортированных по национальности.");
+                Console.WriteLine("5) выход;");
                 choice = Console.ReadLine();
 
-                switch(choice)
+                switch (choice)
                 {
                     case "1":
-                        carServiceStation.ServiceNewCustomer();
+                        databaseCriminals.ShowAllCriminals();
                         break;
                     case "2":
-                        carServiceStation.BuySpare();
+                        databaseCriminals.СonclusionOnGrowth();
                         break;
                     case "3":
-                        carServiceStation.ShowWarehouse();
+                        databaseCriminals.ConclusionOnWeight();
                         break;
                     case "4":
-                        isWork = false;
+                        databaseCriminals.ConclusionOnNationality();
                         break;
-                    default:
-                        Console.WriteLine("error");
+                    case "5":
                         break;
                 }
             }
         }
     }
-    
-    class CarServiceStation
+
+    class Criminal
     {
-        private Producer _producer = new Producer();
-        private List<Spare> _warehouse = new List<Spare>();
-        private int _money = 1000;
-
-        public void BuySpare()
-        {
-            Console.WriteLine("выбрать можно из:");
-
-            foreach(Spare spare in _producer.GetSpares())
-            {
-                Console.Write(spare.ToString() + ", ");
-            }
-
-            Console.WriteLine("введите номер выбранной запчасти.");
-            string choiceSpare = Console.ReadLine();
-            
-            if(int.TryParse(choiceSpare, out int result))
-            {
-                if(result > 0 && result <= _producer.GetSpares().Length)
-                {
-                    Spare spare = _producer.GetSpare(result - 1);
-                    if(_money >= spare.Price)
-                    {
-                        _money -= spare.Price;
-                        _warehouse.Add(spare);
-                        Console.WriteLine("Куплена " + spare.Name + ". Денег осталось: " + _money);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Недостаточно денег.");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Ошибка ввода.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Ошибка ввода.");
-            }
-        }
-
-        public void ServiceNewCustomer()
-        {
-            CarCustomer carCustomer = new CarCustomer(_producer);
-            Console.WriteLine("Новая тачка на ремонт! Требуется замена " + carCustomer.NeedSpareName + ". ");
-            
-            if(_warehouse.Contains(carCustomer.Spare)) 
-            {
-                int totalPrice = carCustomer.Spare.PriceReplacement + carCustomer.Spare.Price;
-                Console.WriteLine("на складе есть необходимая деталь. Начинаем замену.");
-                _money += totalPrice;
-                Console.WriteLine("Мы выписали чек: стоимость детали - " + carCustomer.Spare.Price + ", стоимость ремонта - " + carCustomer.Spare.PriceReplacement);
-                Console.WriteLine("Итого: " + totalPrice + ". на счету - " + _money);
-                _warehouse.Remove(carCustomer.Spare);
-            }
-            else
-            {
-                Console.WriteLine("На складе нет этой детали.. Черт, мы упустили клиента..");
-                Console.WriteLine("Дадим ему скидку в  размере 50, чтобы он вернулся.");
-                int discount = 50;
-                _money -= discount;
-                Console.WriteLine("Мы понесли убытки в размере " + discount + ". На счету: " + _money);
-            }
-
-            Console.ReadKey();
-        }
-
-        public void ShowWarehouse()
-        {
-            Console.Clear();
-            Console.WriteLine("На счету: " + _money);
-            Console.WriteLine("----------------------");
-
-            foreach (Spare spare in _warehouse)
-            {
-                Console.WriteLine(spare.Name);
-            }
-
-            Console.WriteLine("----------------------");
-        }
-    }
-
-    class Producer
-    {
-        private static Random _random = new Random();
-        private List<Spare> _spares = new List<Spare>() { new Spare("фара", 70, 20), new Spare("Зеркало", 80, 20), new Spare("Масло", 20, 15),
-            new Spare("Ремень", 10, 30), new Spare("Подшипник", 15, 30), new Spare("Сальник", 10, 25), new Spare("Фильтр", 15, 25) };
-
-        public Spare GetSpare(int varianteSpare)
-        {
-            return _spares[varianteSpare];
-        }
-
-        public Spare GetRandomSpare()
-        {
-            return _spares[_random.Next(0, _spares.Count)];
-        }
-
-        public Spare[] GetSpares()
-        {
-            Spare[] arraySpares = new Spare[_spares.Count];
-            _spares.CopyTo(arraySpares);
-            return arraySpares;
-        }
-    }
-
-    class Spare
-    {
-        private static Random _random = new Random();
-        
         public string Name { get; private set; }
-        public int Price { get; private set; }
-        public int PriceReplacement { get; private set; }
 
-        public Spare(string name, int price, int priceReplacement)
+        public int Growth { get; private set; }
+        public int Weight { get; private set; }
+        public string Nationality { get; private set; }
+        public bool ServingSentence { get; private set; }
+
+        public Criminal(string name, int growth, int weight, string nationality, bool servingSentence)
         {
             Name = name;
-            Price = price;
-            PriceReplacement = priceReplacement;
+            Growth = growth;
+            Weight = weight;
+            Nationality = nationality;
+            ServingSentence = servingSentence;
         }
-
         public override string ToString()
         {
-            return Name;
-        }
+            string serving;
 
-        public override bool Equals(object obj)
-        {
-            if (obj is Spare spare)
+            if (ServingSentence == true)
             {
-                return Name == spare.Name;
+                serving = "пойман";
             }
             else
             {
-                return false;
-            }            
+                serving = "на свободе";
+            }
+
+            return Name + " / " + Growth +  " см. / " + Weight + " кг. / " + "страна - " + Nationality + " / статус - " + serving;
         }
     }
 
-    class CarCustomer
+    class DatabaseCriminals
     {
-        public Spare Spare { get; private set; }
-        public string NeedSpareName { get; private set; }
+        private List<Criminal> _Criminals = new List<Criminal>() {
+        new Criminal("Майкл", 180, 79, "Англия", false),
+        new Criminal("Байрам", 158, 68, "Сербия", true),
+        new Criminal("Джек", 167, 81, "Англия", true),
+        new Criminal("Усама", 172, 85, "Саудовская Аравия", false),
+        new Criminal("Пабло", 161, 70, "Колумбия", true),
+        new Criminal("Джозеф", 172, 85, "Уганда", false),
+        new Criminal("Роберт", 172, 85, "Америка", false),
+        new Criminal("Алексис", 172, 85, "Америка", false)};
 
-        public CarCustomer(Producer producer)
+        public void ShowAllCriminals()
         {
-            Spare = producer.GetRandomSpare();
-            NeedSpareName = Spare.Name;
+            foreach(Criminal criminal in _Criminals)
+            {
+                Console.WriteLine(criminal.ToString());
+            }
+        }
+
+        public void СonclusionOnGrowth()
+        {
+            Console.WriteLine("Ведите минимальный порог роста для поиска.");
+
+            if(int.TryParse(Console.ReadLine(), out int result))
+            {
+                var filteredCriminals = _Criminals.Where(criminal => criminal.Growth >= result).Where(criminal => criminal.ServingSentence == false);
+
+                foreach(Criminal criminal in filteredCriminals)
+                {
+                    Console.WriteLine(criminal.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода");
+            }
+        }
+
+        public void ConclusionOnWeight()
+        {
+            Console.WriteLine("Ведите минимальный порог веса для поиска.");
+
+            if (int.TryParse(Console.ReadLine(), out int result))
+            {
+                var filteredCriminals = _Criminals.Where(criminal => criminal.Weight >= result).Where(criminal => criminal.ServingSentence == false);
+
+                foreach (Criminal criminal in filteredCriminals)
+                {
+                    Console.WriteLine(criminal.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода");
+            }
+        }
+
+        public void ConclusionOnNationality()
+        {
+            Console.WriteLine("Ведите страну для поиска.");
+            string nationality = Console.ReadLine();
+            var filteredCriminals = _Criminals.Where(criminal => criminal.Nationality.ToUpper() == nationality.ToUpper()).Where(criminal => criminal.ServingSentence == false);
+
+            foreach (Criminal criminal in filteredCriminals)
+            {
+                    Console.WriteLine(criminal.ToString());
+            }
         }
     }
 }
