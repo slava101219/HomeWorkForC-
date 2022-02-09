51358a36d5ccc5caea8aab44e3674c1f2f29d475
slava101219 <morozko.slava@mail.ru>
2022-02-09 11:17:31 +0200

    df

--- ConsoleAppForCSLight/Program.cs
+++ ConsoleAppForCSLight/Program.cs
@@ -58,24 +58,24 @@ namespace ConsoleAppForCSLight
 
     class DatabaseSoldiers
     {
-        private List<Soldier> _soldiersWithB = new List<Soldier>();
+        private List<Soldier> _soldiers2 = new List<Soldier>(){
+        new Soldier("Рембо", Rank.лейтенант, Armament.Пистолет, 17),
+        new Soldier("Сталоне", Rank.капитан, Armament.Пулемет, 34),
+        new Soldier("барк", Rank.сержант, Armament.Автомат, 3),
+        new Soldier("Витька", Rank.сержант, Armament.Граната, 8),
+        new Soldier("Вовка", Rank.сержант, Armament.Пистолет, 15) };
         private List<Soldier> _soldiers = new List<Soldier>() {
         new Soldier("Боря", Rank.капитан, Armament.Автомат, 12),
         new Soldier("Деня", Rank.лейтенант, Armament.Граната, 23),
         new Soldier("Макс", Rank.майор, Armament.Пистолет, 6),
         new Soldier("Димас", Rank.сержант, Armament.Пулемет, 5),
-        new Soldier("Колян", Rank.лейтенант, Armament.Граната, 21),
-        new Soldier("Рембо", Rank.лейтенант, Armament.Пистолет, 17),
-        new Soldier("Сталоне", Rank.капитан, Armament.Пулемет, 34),
-        new Soldier("барк", Rank.сержант, Armament.Автомат, 3),
-        new Soldier("Витька", Rank.сержант, Armament.Граната, 8),
-        new Soldier("Вовка", Rank.сержант, Armament.Пистолет, 15)};
+        new Soldier("Колян", Rank.лейтенант, Armament.Граната, 21),};
 
-    public void ShowAllSoldiers()
+    public void ShowAllSoldiers(List <Soldier> soldiers)
         {
             Console.WriteLine("----------------------");
 
-            foreach (Soldier soldier in _soldiers)
+            foreach (Soldier soldier in soldiers)
             {
                 Console.WriteLine(soldier.ToString());
             }
@@ -85,16 +85,13 @@ namespace ConsoleAppForCSLight
 
         public void TransferSoldiers()
         {
-            ShowAllSoldiers();
+            ShowAllSoldiers(_soldiers);
+            ShowAllSoldiers(_soldiers2);
             var sortedSoldierWithB = _soldiers.Where(soldier => soldier.Name.ToUpper().StartsWith("Б")).ToList();
-            _soldiersWithB = sortedSoldierWithB;
-            _soldiers = _soldiers.Except(_soldiersWithB).ToList();
-            ShowAllSoldiers();
-
-            foreach (Soldier soldier in _soldiersWithB)
-            {
-                Console.WriteLine(soldier.ToString());
-            }
+            _soldiers2 = _soldiers2.Union(sortedSoldierWithB).ToList();
+            _soldiers = _soldiers.Except(_soldiers2).ToList();
+            ShowAllSoldiers(_soldiers);
+            ShowAllSoldiers(_soldiers2);
         }
     }
 }
